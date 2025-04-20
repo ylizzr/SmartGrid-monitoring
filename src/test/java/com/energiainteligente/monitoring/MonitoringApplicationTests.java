be5f7f6a763f0a925329cfc63facf2e42db86b1e
ylizzr <ylizarazor@ucentral.edu.co>
2025-04-19 21:03:18 -0500

    first commit

---
+++ src/test/java/com/energiainteligente/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energiainteligente.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
